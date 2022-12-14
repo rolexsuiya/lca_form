4bf55845b4ce0b344e0f00969201180220d71440
rolexsuiya <surya.bca00@gmail.com>
2022-12-14 16:12:44 +0530

    lca edit correction

--- src/screen/Lca_edit.js
+++ src/screen/Lca_edit.js
@@ -212,7 +212,7 @@ const LcaEdit = () => {
     }
   };
 
-  const handleChange = (event = "React.SyntheticEvent", newValue = number) => {
+  const handleChange = (event = "React.SyntheticEvent", newValue = 0) => {
     setValue(newValue);
   };
 
